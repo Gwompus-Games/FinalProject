f63d23782dce8afd5d046133d99a4b9552b3d0f9
TheBirdBob <135292573+TheBirdBob@users.noreply.github.com>
2024-10-15 16:41:49 -0700

    changes to player controller and game manager

--- Game Files/Final Project/Assets/Code/Scripts/Managers/GameManager.cs
+++ Game Files/Final Project/Assets/Code/Scripts/Managers/GameManager.cs
@@ -114,6 +114,11 @@ public class GameManager : MonoBehaviour
             }
         }
 
+        for(int o = 0; o < _managedObjects.Count; o++)
+        {
+            _managedObjects[o].Init();
+        }
+
         GetManagedComponent<PlayerController>().TeleportPlayer(_playerSpawnPoint.position);
     }
 
