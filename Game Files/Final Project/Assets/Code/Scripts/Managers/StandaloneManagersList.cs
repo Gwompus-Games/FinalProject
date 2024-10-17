16d1ea77ece8391b7994c347887fcbb714902d82
TheBirdBob <135292573+TheBirdBob@users.noreply.github.com>
2024-10-17 15:42:00 -0700

    fixing some issues

--- Game Files/Final Project/Assets/Code/Scripts/Managers/StandaloneManagersList.cs
+++ Game Files/Final Project/Assets/Code/Scripts/Managers/StandaloneManagersList.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 public class StandaloneManagersList : MonoBehaviour
 {
     public List<Type> standaloneManagers { get; private set; }
+    public List<Type> managedChildren { get; private set; }
     public void SetUpList()
     {
         standaloneManagers = new List<Type> 
@@ -12,5 +15,7 @@ public class StandaloneManagersList : MonoBehaviour
             typeof(AudioManager),
             typeof(FMODEvents)
         };
+
+        managedChildren = Assembly.GetAssembly(typeof(ManagedByGameManager)).GetTypes().Where(t => t.IsSubclassOf(typeof(ManagedByGameManager))).ToList();
     }
 }
