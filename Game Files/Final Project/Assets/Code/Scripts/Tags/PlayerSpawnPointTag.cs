22099d8bbd49f7b4ca5c8302f4321afe03846393
GD-Asoka <51798666+GD-Asoka@users.noreply.github.com>
2024-10-31 15:03:41 -0700

    game_main scene

--- Game Files/Final Project/Assets/Code/Scripts/Tags/PlayerSpawnPointTag.cs
+++ Game Files/Final Project/Assets/Code/Scripts/Tags/PlayerSpawnPointTag.cs
@@ -2,4 +2,14 @@ using UnityEngine;
 
 public class PlayerSpawnPointTag : MonoBehaviour
 {
+    private void Start()
+    {
+        Invoke(nameof(SetParent), 5f);
+    }
+
+    private void SetParent()
+    {
+        transform.parent = FindObjectOfType<Submarine>().transform;
+        transform.localPosition = Vector3.up;
+    }
 }
